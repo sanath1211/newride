4c6ee9c8565737a532497590631ca3d93afd4c47
sanath1211 <84760690+sanath1211@users.noreply.github.com>
2024-04-15 01:39:27 +0530

    commit

--- Apps/Screens/Club/EventItem.js
+++ Apps/Screens/Club/EventItem.js
@@ -6,7 +6,7 @@ import { useNavigation } from '@react-navigation/native'
 export default function EventItem({id,test,description}) {
     const navigation=useNavigation()
   return (
-    <TouchableOpacity onPress={()=>navigation.navigate('event',{eventId:id})} style={{borderWidth:1,borderColor:'#c5c5c5',
+    <TouchableOpacity onPress={()=>navigation.navigate('event',{eventId:id,title:test,description:description})} style={{borderWidth:1,borderColor:'#c5c5c5',
     borderRadius:10,marginVertical:5,padding:30}}>
         <Text>{test}</Text>
         <Text>{description}</Text>
--- Apps/Screens/Club/Events.js
+++ Apps/Screens/Club/Events.js
@@ -1,10 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native'
+import Header from '../HomeScreen/Header'
 
 export default function Events() {
+    const route=useRoute()
+    const {eventId,title,description}=route.params
+   
   return (
-    <View style={{padding:20}}>
-      <Text>Events</Text>
+    <View>
+        <Header/>
+        <View style={{borderWidth:1,borderColor:'#c5c5c5',
+    borderRadius:10,marginVertical:5,padding:20,margin:20}}>
+            <Text>This id event detail screen of {eventId}</Text>
+            <Text>{title}</Text>
+            <Text>{description}</Text>
+        </View>
     </View>
   )
 }
